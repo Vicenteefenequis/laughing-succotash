0323cd324d68452f587953f1cdb27d047727e723
vicente <vicente19981@live.com>
2021-10-31 15:15:27 -0300

    refactor: change message error in repository

--- internal/repositories/user-repository.go
+++ internal/repositories/user-repository.go
@@ -1,8 +1,10 @@
 package repositories
 
 import (
+	"github.com/matiasvarela/errors"
 	"gorm.io/gorm"
 	"laughing-succostash/internal/core/domain"
+	"laughing-succostash/pkg/apperrors"
 )
 
 type User struct {
@@ -19,13 +21,13 @@ func (r *User) Save(u domain.User) (*domain.User, error) {
 
 	var _user domain.User
 
-	r.db.First(&_user,"id = ?", u.ID)
+	r.db.First(&_user, "id = ?", u.ID)
 
 	if _user.ID != "" {
 		err := r.db.Model(&_user).Updates(u).Error
 
 		if err != nil {
-			return nil, err
+			return &domain.User{}, errors.New(apperrors.IllegalOperation, err, "failed", "fail to update user on database")
 		}
 
 		return &u, nil
@@ -34,7 +36,7 @@ func (r *User) Save(u domain.User) (*domain.User, error) {
 	err := r.db.Create(&u).Error
 
 	if err != nil {
-		return nil, err
+		return &domain.User{}, errors.New(apperrors.IllegalOperation, err, "failed", "failed save user on database")
 	}
 
 	return &u, nil
@@ -45,5 +47,13 @@ func (r *User) Delete(id string) error {
 }
 
 func (r *User) Get(id string) (*domain.User, error) {
-	return nil, nil
+	var user domain.User
+
+	tx := r.db.First(&user, "id = ?", id)
+
+	if user.ID != "" {
+		return &user, nil
+	}
+
+	return &domain.User{}, errors.New(apperrors.IllegalOperation, tx.Error, "User does not exists", "failed to get user on database")
 }
