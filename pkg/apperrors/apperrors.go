6c157107a6f74e1176c0ef42aaa0842a9b105443
vicente <vicente19981@live.com>
2021-10-31 15:16:43 -0300

    feat: create empty result variable in pkg apperros

--- pkg/apperrors/apperrors.go
+++ pkg/apperrors/apperrors.go
@@ -7,6 +7,7 @@ import (
 var (
 	NotFound         = errors.Define("not_found")
 	IllegalOperation = errors.Define("illegal_operation")
-	InvalidInput      = errors.Define("invalid_input")
+	InvalidInput     = errors.Define("invalid_input")
 	Internal         = errors.Define("internal")
+	EmptyResult      = errors.Define("empty_result")
 )
