00353ba5a70151b40fbf3be9497039fe018537f0
vicente <vicente19981@live.com>
2021-11-03 18:53:24 -0300

    refactor: change method get ids param of handlers.go

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,6 +1,19 @@
 package handlers
 
+import "strings"
 
-func buildMessage(key string,data interface{}) map[string]interface{} {
+func buildMessage(key string, data interface{}) map[string]interface{} {
 	return map[string]interface{}{key: data}
+}
+
+func getIdsParam(idsParam string) []string {
+	var ids []string
+
+	if idsParam != "" {
+		for _, id := range strings.Split(idsParam, ",") {
+			ids = append(ids, id)
+		}
+	}
+
+	return ids
 }
