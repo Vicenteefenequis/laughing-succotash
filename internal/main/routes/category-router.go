9a38ee4d9482be1ee60128cfdb519dbed0a7eb72
vicente <vicente19981@live.com>
2021-11-03 19:26:39 -0300

    feat: create abstract func of category routers

---
+++ internal/main/routes/category-router.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+	"laughing-succostash/internal/core/service"
+	"laughing-succostash/internal/handlers"
+	"laughing-succostash/internal/repositories"
+)
+
+type Category struct {
+	e  *echo.Echo
+	db *gorm.DB
+}
+
+func NewCategoryRouter(db *gorm.DB, e *echo.Echo) *Category {
+	return &Category{
+		db: db,
+		e:  e,
+	}
+}
+
+func (u *Category) CategoryRouter() {
+
+	repoCategory := repositories.NewCategoryRepository(u.db)
+	serviceCategory := service.NewCategoryService(repoCategory)
+	handlerCategory := handlers.NewCategoryHttpHandler(serviceCategory)
+
+	categoryRoutes := u.e.Group("category")
+	categoryRoutes.POST("", handlerCategory.Create)
+	categoryRoutes.GET("", handlerCategory.FindAll)
+	categoryRoutes.GET("/:id", handlerCategory.Get)
+	categoryRoutes.DELETE("/:id", handlerCategory.Delete)
+	categoryRoutes.PUT("", handlerCategory.Update)
+}
