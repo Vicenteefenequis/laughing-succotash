e4a52a63c5e65ac25a6e37ca696788fcd77e27fd
vicente <vicente19981@live.com>
2021-11-03 18:52:03 -0300

    refactor: change service port of receive slices ids

--- internal/core/ports/service/category.go
+++ internal/core/ports/service/category.go
@@ -4,8 +4,8 @@ import "laughing-succostash/internal/core/domain"
 
 type Category interface {
 	Create(category domain.Category) (*domain.Category, error)
-	FindAll() ([]domain.Category, error)
-	FindOne(id string) (*domain.Category, error)
+	FindAll(ids []string) ([]domain.Category, error)
+	FindOne(ids []string) (*domain.Category, error)
 	Delete(id string) error
 	Update(Category domain.Category) (*domain.Category, error)
 }
