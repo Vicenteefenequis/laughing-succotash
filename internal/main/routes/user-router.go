a879458877fde0f21d89a101edb1a0f990e13622
vicente <vicente19981@live.com>
2021-11-03 19:26:51 -0300

    feat: create abstract func of user routers

---
+++ internal/main/routes/user-router.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+	"laughing-succostash/internal/core/service"
+	"laughing-succostash/internal/handlers"
+	"laughing-succostash/internal/repositories"
+)
+
+type User struct {
+	e  *echo.Echo
+	db *gorm.DB
+}
+
+func NewUserRouter(db *gorm.DB, e *echo.Echo) *User {
+	return &User{
+		db: db,
+		e:  e,
+	}
+}
+
+func (u *User) UserRouter() {
+
+	repoUser := repositories.NewUserRepository(u.db)
+	serviceUser := service.NewUserService(repoUser)
+	handlerUser := handlers.NewUserHttpHandler(serviceUser)
+
+	userRoutes := u.e.Group("/user")
+	userRoutes.POST("", handlerUser.Create)
+	userRoutes.GET("", handlerUser.FindAll)
+	userRoutes.GET("/:id", handlerUser.Get)
+	userRoutes.DELETE("/:id", handlerUser.Delete)
+	userRoutes.PUT("", handlerUser.Update)
+}
