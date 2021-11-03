4a56331c3804537e2ed44fcca61d355dc662a2ab
vicente <vicente19981@live.com>
2021-11-02 23:59:42 -0300

    refactor: change method get and remove findall of user repository

--- internal/core/ports/repositories/user.go
+++ internal/core/ports/repositories/user.go
@@ -6,7 +6,6 @@ import (
 
 type User interface {
 	Save(user domain.User) (*domain.User, error)
-	Get(id string) (*domain.User, error)
-	FindAll() ([]domain.User, error)
+	Get(ids []string) ([]domain.User, error)
 	Delete(id string) error
 }
