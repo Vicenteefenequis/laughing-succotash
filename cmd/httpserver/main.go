b50bc985c1c72e326c7a2b2e738edcf7028e04f2
vicente <vicente19981@live.com>
2021-11-03 19:27:03 -0300

    refactor: usage abstracts functions

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -3,11 +3,8 @@ package main
 import (
 	env "github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"laughing-succostash/internal/core/database"
-	"laughing-succostash/internal/core/service"
-	"laughing-succostash/internal/handlers"
-	"laughing-succostash/internal/repositories"
+	"laughing-succostash/internal/main/routes"
 	"log"
 )
 
@@ -21,33 +18,13 @@ func init() {
 func main() {
 
 	db := database.NewDatabase().Connect()
-
-	repoUser := repositories.NewUserRepository(db)
-	serviceUser := service.NewUserService(repoUser)
-	handlerUser := handlers.NewUserHttpHandler(serviceUser)
-
-	repoCategory := repositories.NewCategoryRepository(db)
-	serviceCategory := service.NewCategoryService(repoCategory)
-	handlerCategory := handlers.NewCategoryHttpHandler(serviceCategory)
-
 	e := echo.New()
 
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	userRouter := routes.NewUserRouter(db, e)
+	userRouter.UserRouter()
 
-	userHandler := e.Group("/user")
-	userHandler.POST("", handlerUser.Create)
-	userHandler.GET("", handlerUser.FindAll)
-	userHandler.GET("/:id", handlerUser.Get)
-	userHandler.DELETE("/:id", handlerUser.Delete)
-	userHandler.PUT("", handlerUser.Update)
-
-	categoryHandler := e.Group("category")
-	categoryHandler.POST("", handlerCategory.Create)
-	categoryHandler.GET("", handlerCategory.FindAll)
-	categoryHandler.GET("/:id", handlerCategory.Get)
-	categoryHandler.DELETE("/:id", handlerCategory.Delete)
-	categoryHandler.PUT("", handlerCategory.Update)
+	categoryRouter := routes.NewCategoryRouter(db, e)
+	categoryRouter.CategoryRouter()
 
 	e.Logger.Fatal(e.Start(":8080"))
 
