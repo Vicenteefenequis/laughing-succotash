5abe68a19fcb73d3b0b131237f55893bdd3c2ed5
vicente <vicente19981@live.com>
2021-11-09 23:35:50 -0300

    refactor: removing find one and overflow one method find

--- internal/core/ports/service/category.go
+++ internal/core/ports/service/category.go
@@ -4,8 +4,7 @@ import "laughing-succostash/internal/core/domain"
 
 type Category interface {
 	Create(category domain.Category) (*domain.Category, error)
-	FindAll(ids []string, limit int, offset int) ([]domain.Category, error)
-	FindOne(ids []string) (*domain.Category, error)
+	Find(ids []string, limit int, offset int) ([]domain.Category, error)
 	Delete(id string) error
 	Update(Category domain.Category) (*domain.Category, error)
 }
