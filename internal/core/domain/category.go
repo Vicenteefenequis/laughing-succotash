ab5b8fd63fa8022a7b4b4243833a7377589ec08d
vicente <vicente19981@live.com>
2021-11-09 23:35:26 -0300

    refactor: omit empty category from products

--- internal/core/domain/category.go
+++ internal/core/domain/category.go
@@ -5,8 +5,9 @@ import (
 )
 
 type Category struct {
-	ID   string `gorm:"primaryKey;type:char(36)" json:"id"`
-	Name string `json:"name"`
+	ID       string    `gorm:"primaryKey;type:char(36)" json:"id"`
+	Name     string    `json:"name"`
+	Products []Product `gorm:"foreignKey:CategoryId" json:"products,omitempty"`
 }
 
 func (c *Category) BeforeSave(tx *gorm.DB) error {
