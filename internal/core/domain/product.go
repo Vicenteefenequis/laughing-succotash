fb239d12e53ba6914ddb77ee2a889a133e9e2189
vicente <vicente19981@live.com>
2021-11-17 22:04:49 -0300

    refactor: showing field category_id and change price data type to uint64

--- internal/core/domain/product.go
+++ internal/core/domain/product.go
@@ -6,8 +6,8 @@ type Product struct {
 	ID         string    `gorm:"primaryKey;type:char(36)" json:"id"`
 	Name       string    `json:"name"`
 	Quantity   int       `json:"quantity"`
-	Price      float64   `json:"price"`
-	CategoryId string    `json:"-"`
+	Price      uint64    `json:"price"`
+	CategoryId string    `json:"category_id"`
 	Category   *Category `json:"category,omitempty"`
 }
 
